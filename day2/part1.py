9cf9ed31df3dad7a12c2486e0f6912be918455a8
justinschuster <schujustin@gmail.com>
2023-12-18 16:19:27 -0500

    Finished part two

--- day2/part1.py
+++ day2/part1.py
@@ -11,7 +11,6 @@ def part_one(puzzle_input):
             game = game.split(': ')[1]
         else:
             break
-
         for hand in game.split('; '):
             imposs = False
             for subset in hand.split(', '):
@@ -23,9 +22,23 @@ def part_one(puzzle_input):
                 break
         else:
             poss_games += id
-
     return poss_games
 
+def part_two(puzzle_input):
+    power = 0
+    for id, game in enumerate(puzzle_input.split('\n'), start=1):
+        max_num = {'red': 0, 'blue': 0, 'green': 0}
+        if game != '':
+            game = game.split(': ')[1]
+        else:
+            break
+        for hand in game.split('; '):
+            for subset in hand.split(', '):
+                n, color = subset.split()
+                max_num[color] = max(int(n), max_num[color])
+        power = power + (max_num['red'] * max_num['blue'] * max_num['green'])
+    return power
+
 if __name__ == "__main__":
-    puzzle_input = get_input()
-    print('Part One: ', part_one(puzzle_input))
+    print('Part One: ', part_one(get_input()))
+    print('Part Two: ', part_two(get_input()))
